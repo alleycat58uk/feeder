e0700bfff58660edd707dbcb9498a346c2ff7a0d
Paul Griffiths <paul@adt.org.uk>
2018-01-24 21:42:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+from requests import RequestException
+
+
+# feed urls
+# feed = "http://rss.dw.com/atom/rss-en-all"
+feed = "http://rss.dw.com/rdf/rss-en-all"
+# fetch news items
+try:
+    r = requests.get(feed)
+except RequestException as e:
+    print('Error fetching website')
+else:
+    soup = BeautifulSoup(r.text, "xml")
+    print(soup.title.contents[0])
+    items = soup.find_all("item")
+    for item in items:
+        print(item.title.contents[0])
+        print(item.description.contents[0])
+        print(item.link.contents[0])
+        print(item.subject.contents[0])
+        print(item.date.contents[0], "\n")
