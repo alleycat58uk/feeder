cb070f7da08e5e2d7a9fef430e2909bc08cd9548
alleycat58uk <paul@adt.org.uk>
2018-01-29 16:22:24 +0000

    Removed capitalisation og function names and arguments

--- feeder/feeds.py
+++ feeder/feeds.py
@@ -1,25 +1,25 @@
-def addFeed(name,url,region,subject,filter,dateFormat):
+def addFeed(name,url,region,subject,filter,dateformat):
     pass
 
 
-def getAllFeeds():
+def getallfeeds():
     return ["http://www.france24.com/en/top-stories/rss",
             "http://rss.dw.com/rdf/rss-en-all",
             "http://feeds.bbci.co.uk/news/rss.xml",
             "https://feeds.feedburner.com/Torrentfreak"]
 
 
-def getFeedItems(feed):
+def getfeeditems(feed):
     pass
 
 
-def checkItem(date,url):
+def checkitem(date,url):
     pass
 
 
-def formatDate(dateFormat):
+def formatdate(dateFormat):
     pass
 
 
-def saveItem(name,url,region,subject,filter,dateFormat):
+def saveitem(name,url,region,subject,filter,dateFormat):
     pass
