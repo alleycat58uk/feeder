3fa95d1ea639dcb41a8fbebb7664fb4d1f44ff20
Paul Griffiths <paul@adt.org.uk>
2018-02-12 18:23:00 +0000

    change of function name to get all feeds

--- main.py
+++ main.py
@@ -6,55 +6,56 @@ from feeder import feeds
 
 
 # get feed urls
-feed = feeds.getFeeds()
+feeds = feeds.getAllFeeds()
 
 # fetch news items
-try:
-    r = requests.get(feed[0])
-except RequestException as e:
-    print('Error fetching website')
-else:
-    # set tag name lists for parsing news items
-    titleTag = ['title']
-    descriptionTag = ['description']
-    linkTag = ['link']
-    subjectTag = ['subject', 'category']
-    pubDateTag = ['date', 'pubDate']
-    # parse document
-    soup = BeautifulSoup(r.text, "xml")
-    feedTitle = soup.title.contents[0]
-    items = soup.find_all("item")
-    for item in items:
-        if item.find(titleTag):
-            itemTitle = item.find(titleTag).contents[0]
-        else:
-            itemTitle = None
-        if item.find(descriptionTag):
-            itemDesc = item.find(descriptionTag).contents[0]
-        else:
-            itemDesc = None
-        if item.find(linkTag):
-            itemLink = item.find(linkTag).contents[0]
-        else:
-            itemLink = None
-        if item.find(subjectTag):
-            itemSubjects = []
-            for subject in item.find_all(subjectTag):
-                itemSubjects.append(subject.contents[0])
-        else:
-            itemSubjects = None
-        if item.find(pubDateTag):
-            itemPubDate = item.find(pubDateTag).contents[0]
-        else:
-            itemPubDate = None
+for feed in feeds:
+    try:
+        r = requests.get(feed)
+    except RequestException as e:
+        print('Error fetching website')
+    else:
+        # set tag name lists for parsing news items
+        titleTag = ['title']
+        descriptionTag = ['description']
+        linkTag = ['link']
+        subjectTag = ['subject', 'category']
+        pubDateTag = ['date', 'pubDate']
+        # parse document
+        soup = BeautifulSoup(r.text, "xml")
+        feedTitle = soup.title.contents[0]
+        items = soup.find_all("item")
+        for item in items:
+            if item.find(titleTag):
+                itemTitle = item.find(titleTag).contents[0]
+            else:
+                itemTitle = None
+            if item.find(descriptionTag):
+                itemDesc = item.find(descriptionTag).contents[0]
+            else:
+                itemDesc = None
+            if item.find(linkTag):
+                itemLink = item.find(linkTag).contents[0]
+            else:
+                itemLink = None
+            if item.find(subjectTag):
+                itemSubjects = []
+                for subject in item.find_all(subjectTag):
+                    itemSubjects.append(subject.contents[0])
+            else:
+                itemSubjects = None
+            if item.find(pubDateTag):
+                itemPubDate = item.find(pubDateTag).contents[0]
+            else:
+                itemPubDate = None
 
-        # store news item
-        payload = json.dumps({'type': 'item',
-                              'feed': {'title': feedTitle},
-                              'item': {'title': itemTitle,
-                                       'description': itemDesc,
-                                       'link': itemLink,
-                                       'subjects': itemSubjects,
-                                       'pubDate': itemPubDate}})
-        rt = requests.post('http://127.0.0.1:5984/feeder', data=payload, headers={'Content-Type': 'application/json'})
-        print(rt)
+            # store news item
+            payload = json.dumps({'type': 'item',
+                                  'feed': {'title': feedTitle},
+                                  'item': {'title': itemTitle,
+                                           'description': itemDesc,
+                                           'link': itemLink,
+                                           'subjects': itemSubjects,
+                                           'pubDate': itemPubDate}})
+            rt = requests.post('http://127.0.0.1:5984/feeder', data=payload, headers={'Content-Type': 'application/json'})
+            print(rt)
