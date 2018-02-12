438c5fddc92fa2d93bd594878f9a562d139db7a2
alleycat58uk <paul@adt.org.uk>
2018-02-12 16:43:34 +0000

    Loop through available feeds

--- feeder/feeds.py
+++ feeder/feeds.py
@@ -1,12 +1,31 @@
-def addFeed(name,url,region,subject,filter,dateformat):
+import requests
+import json
+
+
+def addfeed(name,url,region,subject,filter,dateformat):
     pass
 
 
 def getallfeeds():
-    return ["http://www.france24.com/en/top-stories/rss",
-            "http://rss.dw.com/rdf/rss-en-all",
-            "http://feeds.bbci.co.uk/news/rss.xml",
-            "https://feeds.feedburner.com/Torrentfreak"]
+    url = "http://127.0.0.1:5984/feeder/_find"
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    payload = json.dumps({
+        "selector": {
+            "type": {
+                "$eq": "feed"
+            }
+        },
+        "fields": [
+            "name",
+            "url",
+            "region",
+            "subject",
+            "filter",
+            "date-format"
+        ]
+    })
+    r = requests.post(url, headers=headers, data=payload)
+    return json.loads(r.text)['docs']
 
 
 def getfeeditems(feed):
