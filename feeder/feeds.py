20c4bd5cb642ce93d42b142d4226d10895cba5fa
alleycat58uk <paul@adt.org.uk>
2018-03-02 11:32:22 +0000

    Added first accessed date to new record and comment placeholder for cheking if a record exists

--- feeder/feeds.py
+++ feeder/feeds.py
@@ -28,8 +28,50 @@ def getallfeeds():
     return json.loads(r.text)['docs']
 
 
-def getfeeditems(feed):
-    pass
+def getfeeditem(item,feedname,feedregion,feedsubject,feedfilter):
+    # set tag name lists for parsing news items
+    titletag = ['title']
+    descriptiontag = ['description']
+    linktag = ['link']
+    subjecttag = ['subject', 'category']
+    pubdatetag = ['date', 'pubDate']
+    # extract required item details
+    if item.find(titletag):
+        itemtitle = item.find(titletag).contents[0]
+    else:
+        itemtitle = None
+    if item.find(descriptiontag):
+        itemdesc = item.find(descriptiontag).contents[0]
+    else:
+        itemdesc = None
+    if item.find(linktag):
+        itemlink = item.find(linktag).contents[0]
+    else:
+        itemlink = None
+    if item.find(subjecttag):
+        itemsubjects = []
+        for subject in item.find_all(subjecttag):
+            itemsubjects.append(subject.contents[0])
+    else:
+        itemsubjects = None
+    if item.find(pubdatetag):
+        itempubdate = item.find(pubdatetag).contents[0]
+    else:
+        itempubdate = None
+    return json.dumps({'type': 'item',
+                      'accessed': {
+                          'first': 'today',
+                          'last': 'today'
+                      },
+                      'feed': {'title': feedname,
+                               'region': feedregion,
+                               'subject': feedsubject,
+                               'filter': feedfilter},
+                      'item': {'title': itemtitle,
+                               'description': itemdesc,
+                               'link': itemlink,
+                               'subjects': itemsubjects,
+                               'pubDate': itempubdate}})
 
 
 def checkitem(date,url):
--- main.py
+++ main.py
@@ -1,73 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from requests import RequestException
-import json
 from feeder import feeds
 
 
 # get feed urls
-feeds = feeds.getallfeeds()
+allfeeds = feeds.getallfeeds()
 
 # fetch news items
-for feed in feeds:
+for feed in allfeeds:
     try:
         r = requests.get(feed['url'])
     # TODO: include proper, inclusive exception handling
     except RequestException as e:
         print('Error fetching website')
     else:
-        # set tag name lists for parsing news items
-        titleTag = ['title']
-        descriptionTag = ['description']
-        linkTag = ['link']
-        subjectTag = ['subject', 'category']
-        pubDateTag = ['date', 'pubDate']
         # parse document
         soup = BeautifulSoup(r.text, "xml")
         items = soup.find_all("item")
         for item in items:
-            if item.find(titleTag):
-                itemTitle = item.find(titleTag).contents[0]
-            else:
-                itemTitle = None
-            if item.find(descriptionTag):
-                itemDesc = item.find(descriptionTag).contents[0]
-            else:
-                itemDesc = None
-            if item.find(linkTag):
-                itemLink = item.find(linkTag).contents[0]
-            else:
-                itemLink = None
-            if item.find(subjectTag):
-                itemSubjects = []
-                for subject in item.find_all(subjectTag):
-                    itemSubjects.append(subject.contents[0])
-            else:
-                itemSubjects = None
-            if item.find(pubDateTag):
-                itemPubDate = item.find(pubDateTag).contents[0]
-            else:
-                itemPubDate = None
-
-            # check if item already exists
-            # - criteria: feed title, url, itemTitle
-            # no - create record
-            # yes - amend (inc. last date accessed)
-
-            # store new item
-            payload = json.dumps({'type': 'item',
-                                  'accessed': {
-                                      'first': 'today',
-                                      'last': 'today'
-                                  },
-                                  'feed': {'title': feed['name'],
-                                           'region': feed['region'],
-                                           'subject': feed['subject'],
-                                           'filter': feed['filter']},
-                                  'item': {'title': itemTitle,
-                                           'description': itemDesc,
-                                           'link': itemLink,
-                                           'subjects': itemSubjects,
-                                           'pubDate': itemPubDate}})
+            payload = feeds.getfeeditem(item,feed['name'],feed['region'],feed['subject'],feed['filter'])
             rt = requests.post('http://127.0.0.1:5984/feeder', data=payload, headers={'Content-Type': 'application/json'})
             print(rt)
