09d81345453e501a73a83b0d11876a0d9a6c45e4
alleycat58uk <paul@adt.org.uk>
2024-11-16 15:58:44 +0000

    corrected typo

--- config_blank.py
+++ config_blank.py
@@ -5,7 +5,7 @@ sql_ro_user = "sql_read_only_username"
 sql_ro_pass = "sql_read_only_password"
 
 sql_rw_user = "sql_read_write_username"
-rsql_w_pass = "sql_read_write_password"
+sql_rw_pass = "sql_read_write_password"
 
 sql_db_name = "sql_db_name"
 
